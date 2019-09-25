2b2582f2364fa37027f9a8883434195148b693c7
Cameron Risch <risch.cameron@gmail.com>
2019-09-25 11:56:58 -0500

    Commited to finish this

--- app/src/main/java/edu/uwp/cs/csci323/geoquiz/CheatActivity.java
+++ app/src/main/java/edu/uwp/cs/csci323/geoquiz/CheatActivity.java
@@ -1,11 +1,17 @@
 package edu.uwp.cs.csci323.geoquiz;
 
+// Imports Statements
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,6 +23,9 @@ public class CheatActivity extends AppCompatActivity {
     private boolean mAnswerIsTrue;
     private TextView mAnswerTextView;
     private Button mShowAnswerButton;
+    private static final String KEY_INDEX5 = "worknow";
+    private static boolean mIsCheater;
+    private TextView mApiTextView;
 
     public static Intent newIntent(Context packageContext, Boolean answerIsTrue){
         Intent intent = new Intent(packageContext, CheatActivity.class);
@@ -33,30 +42,70 @@ public class CheatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+        if (savedInstanceState != null){
+
+            // TODO Challenge Chapter 5 Closing loopholes
+            if (mIsCheater){
+                setAnswerShownResult(true);
+            }
+           mIsCheater = savedInstanceState.getBoolean(KEY_INDEX5,false);
+        }
+
 
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
 
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
 
+        mApiTextView = (TextView) findViewById(R.id.api_level);
+
         mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
 
 
+        // TODO Challenge Chapter 6 Reporting build version
+        mApiTextView.setText("API Level " + Build.VERSION.SDK_INT);
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
+
 
             @Override
             public void onClick(View v){
+                mIsCheater = true;
                 if (mAnswerIsTrue) {
                     mAnswerTextView.setText(R.string.true_button);
                 } else {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswerButton.getWidth() / 2;
+                    int cy = mShowAnswerButton.getHeight() / 2;
+                    float radius = mShowAnswerButton.getWidth();
+                    Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswerButton.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mShowAnswerButton.setVisibility((View.INVISIBLE));
+                }
+
             }
+
         });
     }
     private void setAnswerShownResult(boolean isAnswerShown){
         Intent data = new Intent();
         data.putExtra(EXTRA_ANSWER_SHOWN,isAnswerShown);
         setResult(RESULT_OK,data);
+    }
+    @Override
+    // TODO Challenge chapter 5
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putBoolean(KEY_INDEX5,mIsCheater);
     }
 }
--- app/src/main/java/edu/uwp/cs/csci323/geoquiz/MainActivity.java
+++ app/src/main/java/edu/uwp/cs/csci323/geoquiz/MainActivity.java
@@ -1,11 +1,23 @@
+/*
+Cameron Risch
+Assignment 1
+9/15/19
+CSCI-323
+
+ */
+
+
 package edu.uwp.cs.csci323.geoquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.nfc.Tag;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -13,8 +25,6 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-import java.util.ArrayList;
-import java.util.Iterator;
 
 import javax.xml.datatype.Duration;
 
@@ -22,16 +32,27 @@ public class MainActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
-
-    // chapter two challenge 3
+    // TODO image button challenge chapter 2
     private ImageButton mNextButton;
+
+    // TODO challenge chapter 2 prev button
     private ImageButton mPrevButton;
     private Button mCheatButton;
     private TextView mQuestionTextView;
     private static final String TAG = "MainActivity";
     private static final String KEY_INDEX = "index";
+
+    // extra indexes for saved instances
     private static final String KEY_INDEX2 = "anything";
     private static final String KEY_INDEX3 = "somerthinf";
+    private static final String KEY_INDEX4 = "work";
+    private static final String KEY_INDEX5 = "wjjoijork";
+    private static final String KEY_INDEX6 = "fnajksdf";
+
+    // TODO challenge chapter 6 cheats lefts
+    private int cheatsLeft = 4;
+
+
     private static final int REQUEST_CODE_CHEAT = 0;
     private static int score = 0;
     private int messageId = 0;
@@ -48,7 +69,11 @@ public class MainActivity extends AppCompatActivity {
     };
     private int mCurrentIndex = 0;
     private boolean mIsCheater;
-        private boolean answer[];
+    // TODO chapter 3 challenge preventing repeated answers
+    private boolean answer[];
+
+    // TODO chapter 5 challenge
+    private boolean cheated[];
 
 
     @Override
@@ -56,11 +81,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
+
+        // TODO chapter 3 challenge
         answer= new  boolean[mQuestionBank.length];
+
+        // TODO chapter 5 challenge
+        cheated = new boolean[mQuestionBank.length];
+
+        // TODO saved instance states for challenges
         if (savedInstanceState != null){
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
             answer = savedInstanceState.getBooleanArray(KEY_INDEX2);
             score = savedInstanceState.getInt(KEY_INDEX3, 0);
+            mIsCheater = savedInstanceState.getBoolean(KEY_INDEX4,false);
+            cheated = savedInstanceState.getBooleanArray(KEY_INDEX5);
+            cheatsLeft = savedInstanceState.getInt(KEY_INDEX6,0);
         }
 
         viewById();
@@ -81,6 +116,10 @@ public class MainActivity extends AppCompatActivity {
                 return;
             }
             mIsCheater = CheatActivity.wasAnswerShown(data);
+            if(mIsCheater){
+                cheated[mCurrentIndex] = true;
+            }
+
         }
 
     }
@@ -104,9 +143,14 @@ public class MainActivity extends AppCompatActivity {
     public void onSaveInstanceState(Bundle savedInstanceState){
         super.onSaveInstanceState(savedInstanceState);
         Log.i(TAG, "onSaveInstanceState");
+
+        // TODO saved instance states for challenges
         savedInstanceState.putInt(KEY_INDEX,mCurrentIndex);
         savedInstanceState.putBooleanArray(KEY_INDEX2,answer);
         savedInstanceState.putInt(KEY_INDEX3,score);
+        savedInstanceState.putBoolean(KEY_INDEX4,mIsCheater);
+        savedInstanceState.putBooleanArray(KEY_INDEX5,cheated);
+        savedInstanceState.putInt(KEY_INDEX6,cheatsLeft);
     }
 
     @Override
@@ -139,7 +183,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }
-        //Challenge: Chapter one Challenge one
+        // TODO Challenge: Chapter one Challenge one
         Toast gravity = Toast.makeText(MainActivity.this, messageId, Toast.LENGTH_SHORT);
         gravity.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 170);
         gravity.show();
@@ -148,6 +192,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    // TODO chapter 3 challenge
     private void enable(){
         if(answer[mCurrentIndex]){
             mFalseButton.setEnabled(false);
@@ -155,6 +200,13 @@ public class MainActivity extends AppCompatActivity {
         } else {
             mTrueButton.setEnabled(true);
             mFalseButton.setEnabled(true);
+        }
+    }
+    // TODO chapter 5 challenge
+    private void didCheat(){
+
+        if (cheated[mCurrentIndex]){
+            mIsCheater = true;
         }
     }
     private void score(){
@@ -168,13 +220,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void fillArray(){
-        for (int i = 0; i < mQuestionBank.length; i++) {
-            //answers.add(i, 0);
-        }
-    }
-
-    private void viewById(){
+    private void viewById() {
         mQuestionTextView = (TextView) findViewById(R.id.question_text_window);
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
@@ -183,11 +229,14 @@ public class MainActivity extends AppCompatActivity {
         mCheatButton = findViewById(R.id.cheat_button);
 
     }
+
+    // all the on click listeners
     private void setOnClick(){
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
                 answer [mCurrentIndex]=true;
+                didCheat();
                 checkAnswer(true);
             }
         });
@@ -197,6 +246,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 answer [mCurrentIndex]=true;
+                didCheat();
                 checkAnswer(false);
             }
         });
@@ -212,7 +262,7 @@ public class MainActivity extends AppCompatActivity {
 
                 }else {
                     //mCurrentIndex +=1;
-                    mCurrentIndex=(mCurrentIndex+1)%mQuestionBank.length;
+                    mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                     mIsCheater = false;
                     updateQuestion();
                     enable();
@@ -224,13 +274,23 @@ public class MainActivity extends AppCompatActivity {
         mCheatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (cheatsLeft > 0){
+                    cheatsLeft--;
+                }
                 boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
                 Intent intent = CheatActivity.newIntent(MainActivity.this, answerIsTrue);
+                Toast gravity = Toast.makeText(MainActivity.this, "You have " + cheatsLeft + " cheats left", Toast.LENGTH_SHORT);
+                gravity.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 170);
+                gravity.show();
+                if (cheatsLeft == 0){
+                    mCheatButton.setEnabled(false);
+                }
                 startActivityForResult(intent, REQUEST_CODE_CHEAT);
+
             }
         });
 
-        // Chapter two challenge one
+        // TODO Chapter two challenge one
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -248,7 +308,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // chapter 2 challenge 2
+        // TODO chapter 2 challenge 2 prev button
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
